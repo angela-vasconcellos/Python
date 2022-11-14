9908ee2fee1cfdc4d8b6c6092bcf7753ef47dba7
angela-vasconcellos <116106336+angela-vasconcellos@users.noreply.github.com>
2022-11-14 12:26:07 -0300

    Create sql_hist.py

---
+++ sql_hist.py
@@ -0,0 +1,12 @@
+class DevelopmentConfig():
+    DEBUG= True
+    
+    MYSQL_HOST = 'ip'
+    MYSQL_USER = 'user'
+    MYSQL_PASSWORD = 'senha'
+    MYSQL_DB = 'historico'
+
+config_inventario = {
+    'development': DevelopmentConfig   
+
+}
